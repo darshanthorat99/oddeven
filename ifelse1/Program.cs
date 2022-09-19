fc4b2f129ea867cc7cd88bd47b602577a2a6738f
darshanthorat99 <113414840+darshanthorat99@users.noreply.github.com>
2022-09-19 17:02:43 +0530

    Add project files.

---
+++ ifelse1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifelse1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the number");
+            int num=Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("number is even");
+            }
+            else if (num % 2 == 1)
+            {
+              Console.WriteLine("number is odd");
+            }
+            else
+            {
+              Console.WriteLine("input is wrong");
+            }
+
+        }
+    }
+}
